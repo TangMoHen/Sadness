badf53408c3f6768f03541057411908ea2335532
TangMoHen <1401246860@qq.com>
2017-07-05 11:05:49 +0800

    hhh

--- js/index.js
+++ js/index.js
@@ -1,29 +1,13 @@
 window.onload=function(){
-    document.documentElement.style.fontSize=100*document.documentElement.clientWidth/320+'px';
-    window.onresize=function(){
-       document.documentElement.style.fontSize=100*document.documentElement.clientWidth/320+'px'
-    }
-    let aFun=document.querySelector('.hand-r');
-    let oFun=document.querySelector('.function');
-    aFun.onclick=function(){
-        if(oFun.style.display=="block"){
-             oFun.style.display="none";
-        }else{
-            oFun.style.display="block";
-        }
-    };
-    let oUse=document.querySelector('.hand-l');
-    let oUser=document.querySelector('.user');
-    let oHand=document.querySelector('.hand');
-    oUse.onclick=function(){
-        if (oUser.style.marginLeft=="-3.2rem") {
-            oUser.style.marginLeft="-.8rem";
-        }else {
-            oUser.style.marginLeft="-3.2rem";
-        }
-    };
+	let oLogin=document.getElementById('login');
+	let oLgin=document.getElementById('login-z');
+	oLgin.onclick=function(){
+		oLogin.style.display='block';
+	};
 
-    // document.onclick=function(){
-    //     oUser.style.display="none";
-    // }
-}
+	var oDiv=document.getElementById('recharge ');
+	var oSelect=oDiv.getElementById('sel1');
+	oSelect.onchange=function(){
+		alert('恭喜您'+this.value+'充值成功');
+	}
+};
