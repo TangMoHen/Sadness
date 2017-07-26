8a086c004e5d42019bce8bb16066ce08b066a160
TangMoHen <1401246860@qq.com>
2017-07-26 15:13:22 +0800

    hhh

--- js/search.js
+++ js/search.js
@@ -1,46 +1,13 @@
-/**
- * Created by dell on 2017/6/28.
- */
-var name1 =  sessionStorage.getItem("sou");
-$('#inp').val(name1);
-$('#inp1').text(" '"+name1+"' ");
-if (name !== ""){
-    $('#search').show();
-}else {
-    $('#search').hide();
+window.onload=function(){
+	var oNav=document.getElementById("classification");
+	var aLi=oNav.getElementsByTagName('li');
+	for( var i=0;i<aLi.length;i++){
+		aLi[i].index=i;
+		aLi[i].onclick=function(){
+			for( var i=0;i<aLi.length;i++){
+				aLi[i].className='';
+			}
+			this.className='white';
+		}
+	}
 }
-
-var vm = new Vue({
-    el:"#search",
-    data:{
-        search:[],
-        name:[]
-    }
-});
-name = sessionStorage.getItem("sou");
-var url="https://www.kpmaolv.com/maolv/commodity/search?keyWord=[;commodity_name;:;"+name+";,;commodity_status;:;1;]";
-$.ajax({
-    url:url,
-    type:"POST",
-    dataType:"json",
-    success: function (data) {
-        vm.search = data.data.commodityList;
-        vm.name=name;
-    }
-});
-function search1() {
-    var name=$('#inp').val();
-    sessionStorage.setItem("sou",name);
-    if (name == ""){
-        alert('请输入关键词')
-    }else {
-        window.location.reload();
-    }
-    if (vm.search == undefined){
-        $('#null').show()
-    }
-}
-vm.$watch('search',function(val){
-    var loading = document.getElementById('loading')
-    loading.style.display='none';
-});
--- js/special.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by dell on 2017/6/30.
- */
-window.onload =function () {
-    var loading = document.getElementById('loading');
-    loading.style.display='none';
-};
