86b1136932dfaeaeeb49375fae31017c0dc15f83
TangMoHen <1401246860@qq.com>
2017-08-12 17:38:50 +0800

    hhh

--- js/LearningSteps.js
+++ js/LearningSteps.js
@@ -32,28 +32,26 @@ window.onload=function(){
             alert('错了404');
         }
     });
-    // $.ajax({
-    //     url:`https://www.kpmaolv.com/maolv/cookbook/queryInfo${https}`,
-    //     dataType:'json',
-    //     cache:false,
-    //     type:'post',
-    //     success:function(r){
-    //         console.log(r.data.catagoryinfo[1]);
-    //         if(r.result=='0000'){
-    //             for(var i=0;i<r.data.catagoryinfo.length;i++){
-    //                 $(`
-    //                     <div class="swiper-slide" id="li">
-    //                        <img src="${r.data.catagoryinfo[i].cookbookinfo_img}" alt="">
-    //                     </div>
-    //                 `).appendTo('.swiper-wrapper');
-    //             }
-    //             new Swiper('.swiper-container');
-    //         }
-    //     },
-    //     error:()=>{
-    //         alert('错了404');
-    //     }
-    // });
+    $.ajax({
+        url:`https://www.kpmaolv.com/maolv/cookbook/queryInfo${https}`,
+        dataType:'json',
+        cache:false,
+        type:'post',
+        success:function(r){
+            console.log(r.data.catagoryinfo[1]);
+            if(r.result=='0000'){
+                for(var i=0;i<1;i++){
+                    $(`
+                        <img src="${r.data.catagoryinfo[i].cookbookinfo_img}" alt="">
+                    `).appendTo('.the-dishes-b');
+                }
+                new Swiper('.the-dishes-b');
+            }
+        },
+        error:()=>{
+            alert('错了404');
+        }
+    });
 
 
 
