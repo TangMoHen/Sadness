2f0bb47a0ef668fce6ad2e54eaa6436dd4ec70ac
TangMoHen <1401246860@qq.com>
2017-08-16 16:38:28 +0800

    hhh

--- js/base.js
+++
@@ -1,5 +0,0 @@
-document.documentElement.style.fontSize=100*document.documentElement.clientWidth/1080+'px';
-   window.onresize=function(){
-      document.documentElement.style.fontSize=100*document.documentElement.clientWidth/1080+'px'
-   }
-   //以上是rem布局代码
